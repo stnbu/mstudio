c8814ddf316e5fffd90abbd42488f79bcd7010f1
Mike Burr <mb@unintuitive.org>
2023-07-21 10:25:38 -0600

    YYyy! USA Number Won! +++

--- 20230719.0/make.py
+++ 20230719.0/make.py
@@ -3,8 +3,10 @@ from moviepy.editor import *
 from PIL import Image
 import numpy as np
 
-EXTENSIONS = ["mov", "jpg", "mp3", "m4a", "mp4"]
 RESOLUTION = (1920, 1080)
+IMAGE_FILE_EXTENSIONS = ["png", "jpg"]
+AUDIO_FILE_EXTENSIONS = ["mp3", "wav"]
+VIDEO_FILE_EXTENSIONS = ["mov", "m4a"]
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
@@ -19,23 +21,12 @@ def is_python_name(name):
         return False
     return name.isidentifier()
 
-IMAGE_FILE_EXTENSIONS = ["png", "jpg"]
-AUDIO_FILE_EXTENSIONS = ["mp3", "wav"]
-VIDEO_FILE_EXTENSIONS = ["mov", "m4a"]
-
-
 def set_globals_from_media(directory):
-    """This is 'not advised' as it makes code very confusing.
-    """
-    print(">>>>>> hi")
     values = {}
     for name in os.listdir(directory):
         path = os.path.join(directory, name)
         basename, extension = os.path.splitext(name)
         extension = extension[1:]
-        #if not is_python_name(basename):
-        #    raise Exception(f"Not a python name: {basename}")
-        print(f"{extension} vs {VIDEO_FILE_EXTENSIONS}")
         if extension in IMAGE_FILE_EXTENSIONS:
             values[basename] = ImageFileClip(path)
         elif extension in VIDEO_FILE_EXTENSIONS:
@@ -57,16 +48,6 @@ def center_clip(clip, resolution):
     ])    
     return centered
 
-"""
-# Example Usage
-original = VideoFileClip("some_video.mp4")
-slide = ImageClip("some_image.jpg").set_duration(5)
-resolution = (800, 600)
-
-centered_slide = center_clip(slide, resolution)
-new_clip = concatenate_videoclips([original, centered_slide])
-"""
-
 def calculate_max_resolution(clips):
     if not clips:
         raise Exception("One or more clips required")
@@ -74,11 +55,6 @@ def calculate_max_resolution(clips):
     max_width = max(res[0] for res in resolutions)
     max_height = max(res[1] for res in resolutions)
     return (max_width, max_height)
-
-# Example Usage
-# Assuming you have some clips created using moviepy
-# clips = [VideoFileClip("some_video.mp4"), ImageClip("some_image.jpg")]
-# print(calculate_max_resolution(clips))
 
 def get_max_scale(resolution, target):
     width, height = resolution
@@ -89,32 +65,20 @@ def get_max_scale(resolution, target):
         return target_height / height
 
 clips = set_globals_from_media("./media")
-print(clips)
+# MAGIC: 🪄
 globals().update(clips)
 
 # New variables magically spawned by above.
-#flowers0 = flowers_20230714.set_duration(10).set_fps(24) # .set_duration(10)
+flowers0 = flowers_20230714.set_duration(10).set_fps(24)
 walk0 = walk_20230714.subclip(20, 80)
 walk1 = walk_20230716.subclip(1, 13)
-#walk2 = walk_20230718.subclip(3, 14)
-
-#resolution = calculate_max_resolution([walk0])
-""" print(f">>>>res {resolution}")
-scale = get_max_scale(walk0.size, walk0.size)
-print(f">>>>scl {scale}")
-walk0 = walk0.resize(newsize=scale)
- """
-"""
--    img = Image.open(path)
--    img.thumbnail(dimensions, Image.LANCZOS)
--    return ImageClip(np.array(img)).set_duration(duration).set_fps(24)
-"""                    
+walk2 = walk_20230718.subclip(3, 14)
 
 result = concatenate_videoclips([
     walk0, 
-    #    flowers0,
+    flowers0,
     walk1,
-    #walk2
+    walk2
 ])
 
 result.write_videofile("output.mp4", codec='libx264', fps=24)
